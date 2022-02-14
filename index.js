f33cb9369c7f41e76e84c10d7f6132abdc7a918d
Paolo Cargnin <paolo.cargnin@h-is.com>
2022-02-14 12:32:55 +0100

    Params

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const todos = [{
+  id: '38347',
+  message: 'Cook the carbonara',
+  completed: false
+}]
+
+app.set("view engine", "ejs")
+
+// list of the todos
+app.get('/', (req,res) => {
+  res.render('index')
+} ) 
+
+// -> render the details of the todo
+app.get('/todo/:id', (req,res) => {
+  const id = req.params.id
+
+  let todo = null
+  let i = 0
+
+  // find the todo with the same Id
+  while (i < todos.length ){
+    if (todos[i].id == id){
+      todo = todos[i]
+    }
+    i++;
+  }
+  if ( todo ){
+    res.render('todo',{
+      id : id,
+      todo: todo
+    })  
+  }else{
+    res.render('404')
+  }
+})
+
+app.listen(3000)
